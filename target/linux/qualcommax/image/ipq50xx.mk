8a3001f3ef6a3f3f716de79e3a08ca833b8fda5e
crypt0nX <sapien.ok@outlook.com>
2025-12-18 14:09:19 +0800

    add support for wifi, 5g is not working currently

--- target/linux/qualcommax/image/ipq50xx.mk
+++ target/linux/qualcommax/image/ipq50xx.mk
@@ -24,27 +24,13 @@ define Device/xiaomi_ax3000
   SOC := ipq5018
   DEVICE_VENDOR := Xiaomi
   DEVICE_MODEL := AX3000
-  DEVICE_ALT0_VENDOR := Xiaomi
-  DEVICE_ALT0_MODEL := CR880x
-  DEVICE_ALT0_VARIANT := (M81 version)
-  DEVICE_ALT1_VENDOR := Xiaomi
-  DEVICE_ALT1_MODEL := CR880x
-  DEVICE_ALT1_VARIANT := (M79 version)
   BLOCKSIZE := 128k
   PAGESIZE := 2048
   DEVICE_DTS_CONFIG := config@mp02.1
-#   IMAGES := nand-factory.ubi
   DEVICE_PACKAGES := \
 	ath11k-firmware-ipq5018-qcn6122 \
 	kmod-ath11k-ahb \
-	kmod-ath11k-pci \
-	wpad-openssl \
-	ethtool \
-	ip-full \
-	ubi-utils \
-	kmod-gpio-button-hotplug \
-	tcpdump \
-	gpiod-tools
+	wpad-basic-wolfssl
 endef
 TARGET_DEVICES += xiaomi_ax3000
 
